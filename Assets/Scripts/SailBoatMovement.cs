c94f8006f5a740c7374b5cf0d3b94645e74c8302
Travis Martin <trvsmartin@gmail.com>
2024-07-06 16:00:36 -0700

    feat: scale particles with speed, juice up speed

--- Assets/Scripts/SailBoatMovement.cs
+++ Assets/Scripts/SailBoatMovement.cs
@@ -6,9 +6,11 @@ public class SailBoatMovement : MonoBehaviour
     public Rigidbody2D rb; // Reference to the Rigidbody2D component
     
     public float rotationSpeed = 100f;
-    public float degrees=0;
-    public float speedMultiplier;
-    public AreaEffector2D waterWind;
+    public float degrees=0; // track degrees to compare boat angle to wind
+    public float speedMultiplier; // based on degrees (points of sail)
+    public AreaEffector2D waterWind; // base scene area effector; speed of 1
+
+    public ParticleSystem[] wakeEffect;
 
     Vector2 movement; // Vector to store the movement direction
 
@@ -24,22 +26,33 @@ public class SailBoatMovement : MonoBehaviour
         }
         speedMultiplier = degrees switch
         {
-            var d when (d > -30 && d < 30)||(d>330)||(d<-330) => 0.1f,
-            var d when (d >= 30 && d < 60) || (d > -60 && d <= -30)||(d>=270)||(d<=-270) => 0.75f,
-            var d when (d >= 60 && d < 120) || (d > -120 && d <= -60)|| (d>=210)||(d<=-210)=> 1.5f,
-            var d when (d >= 120 && d < 150) || (d > -150 && d <= -120) ||(d>=150)||(d<=-150)=> 1f,
+            var d when (d > -30 && d < 30)||(d>330)||(d<-330) => 0.3f,
+            var d when (d >= 30 && d < 60) || (d > -60 && d <= -30)||(d>=270)||(d<=-270) => 1f,
+            var d when (d >= 60 && d < 120) || (d > -120 && d <= -60)|| (d>=210)||(d<=-210)=> 2f,
+            var d when (d >= 120 && d < 150) || (d > -150 && d <= -120) ||(d>=150)||(d<=-150)=> 1.5f,
             _ => 0.1f
         };
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        foreach(var effect in wakeEffect){
+
+        if (effect != null)
+            {
+                var main = effect.main;
+                main.startSize = new ParticleSystem.MinMaxCurve(Mathf.Clamp(2*speedMultiplier * 1f, 0.1f, 1f));
+                // Optionally, adjust other properties
+                var emission = effect.emission;
+                emission.rateOverTime = speedMultiplier * 20f; // Adjust multiplier as needed
+
+                main.startLifetime = new ParticleSystem.MinMaxCurve(Mathf.Clamp(speedMultiplier * 0.5f, 0.5f, 2f));
+            }
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 forceDirection = rb.transform.up;  // The direction of the force
-        
-
     
         rb.AddForce(forceDirection.normalized * (moveSpeed*speedMultiplier), ForceMode2D.Impulse);
         
@@ -48,7 +61,6 @@ public class SailBoatMovement : MonoBehaviour
         degrees+=rotationAmount;
         Debug.Log($"{degrees}, {speedMultiplier}");
         
-
         // Apply the rotation to the Rigidbody2D
         rb.MoveRotation(rb.rotation + rotationAmount);
 
--- Assets/Scripts/health.cs
+++ Assets/Scripts/health.cs
@@ -12,6 +12,7 @@ public class health : MonoBehaviour
     public GameObject boat;
     public string hearts;
     public GameObject gameOverUI;
+    public GUIStyle healthStyle=new GUIStyle();
    Rect rect = new Rect(0, 0, 300, 100);
    Vector3 offset = new Vector3(0f, 0f, 0.5f); // height above the target position
  
@@ -21,14 +22,15 @@ public class health : MonoBehaviour
         rect.x = point.x;
         rect.y = Screen.height - point.y - rect.height; // bottom left corner set to the 3D point
         hearts=string.Join("", Enumerable.Repeat("♥",currHealth));
-        GUI.Label(rect, $"Health: {hearts}");
+        GUI.Label(rect, $"Health: {hearts}", healthStyle);
     }
     // Start is called before the first frame update
     void Start()
     {
-   boat = GameObject.FindWithTag("Player");
-   gameOverUI.SetActive(false);
-   currHealth=maxHealth;        
+    boat = GameObject.FindWithTag("Player");
+    gameOverUI.SetActive(false);
+    currHealth=maxHealth;
+    healthStyle.fontSize=20;   
     }
 
     // Update is called once per frame
