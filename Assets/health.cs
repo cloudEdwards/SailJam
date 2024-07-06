d544b31686c854f3340ebe74ea963b726690461f
Travis Martin <trvsmartin@gmail.com>
2024-07-06 10:23:41 -0700

    feat: add collision, tickdown health on collision

--- Assets/health.cs
+++ Assets/health.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class health : MonoBehaviour
 {
     public int currHealth=0;
     public int maxHealth=3;
     public GameObject boat;
+    public string hearts;
    Rect rect = new Rect(0, 0, 300, 100);
    Vector3 offset = new Vector3(0f, 0f, 0.5f); // height above the target position
  
@@ -15,24 +17,32 @@ public class health : MonoBehaviour
         Vector3 point = Camera.main.WorldToScreenPoint(boat.transform.position + offset);
         rect.x = point.x;
         rect.y = Screen.height - point.y - rect.height; // bottom left corner set to the 3D point
-        GUI.Label(rect, $"Health: {currHealth}"); // display its name, or other string
+        hearts=string.Join("", Enumerable.Repeat("♥",currHealth));
+        GUI.Label(rect, $"Health: {hearts}"); // TODO: display heart containers or something currHealth*♥ + 3-currhealth*♡ {currHealth}
     }
     // Start is called before the first frame update
     void Start()
     {
    boat = GameObject.FindWithTag("Player");
    currHealth=maxHealth;
+//    healthMeter="♥";
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(currHealth==0){
+            // set GUI.Label to 'sinking' with countdown
+            // game over screen
+        }
         if(Input.GetKeyDown(KeyCode.Space)){
             DamagePlayer(1);
         }
     }
     public void DamagePlayer(int damage){
         currHealth-=damage;
+        // healthMeter.insert("♡");
+        // healthMeter.pop();
     }
 }
