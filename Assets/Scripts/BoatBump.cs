a41250d2d16e05a68ee93c1aafaea9bcd5dac2ee
Cloud Edwards <cloud@retreat.guru>
2024-07-07 12:09:15 -0700

    add crashing audio

--- Assets/Scripts/BoatBump.cs
+++ Assets/Scripts/BoatBump.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class BoatBump : MonoBehaviour
 {
-    public AudioSource bump;
+    public List<AudioSource> bumpSounds;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Obstacle")) {
-            // bump.Play();
+            AudioSource bump = bumpSounds[Random.Range(0, bumpSounds.Count)];
+            bump.Play();
         }
     }
 }
