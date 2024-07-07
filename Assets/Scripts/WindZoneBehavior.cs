044e0d934932cbb2177de9c051e8272044f0e8c8
Cloud Edwards <cloud@retreat.guru>
2024-07-07 16:04:25 -0700

    make fixes, polishing

--- Assets/Scripts/WindZoneBehavior.cs
+++ Assets/Scripts/WindZoneBehavior.cs
@@ -8,7 +8,6 @@ public class WindZoneBehavior : MonoBehaviour
     public float windDirection;
     public ParticleSystem particleEffect;
     public float windSpeed;
-    private List<ParticleSystem> instantiatedObjects=new List<ParticleSystem>();
 
     public float randomMinTime=30;
     public float randomMaxTime=120;
@@ -47,18 +46,11 @@ public class WindZoneBehavior : MonoBehaviour
 
     void ScheduleNextCall()
     {
+        // Calculate a random interval between 10 and 20 seconds
+        float waitTime = Random.Range(randomMinTime, randomMaxTime);
 
-        // ParticleSystem particlesWind = Instantiate(particleEffect, new Vector3(this.transform.position.x, this.transform.position.y, -10), Quaternion.Euler(new Vector3(0,0,windDirection)));
-        // particlesWind.transform.SetParent(this.transform);
-        // var main =particlesWind.main;
-        // main.simulationSpeed=windSpeed;
-        // particlesWind.Play();
-        // instantiatedObjects.Add(particlesWind);
-        // // Calculate a random interval between 10 and 20 seconds
-        // float waitTime = Random.Range(randomMinTime, randomMaxTime);
-
-        // // Schedule the next call
-        // Invoke(nameof(RandonWindDirection), waitTime);
+        // Schedule the next call
+        Invoke(nameof(RandonWindDirection), waitTime);
     }
     
 }
