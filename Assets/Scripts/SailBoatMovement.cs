3bb4083b94df6fa7638e905bcb2accf561b8e983
Travis Martin <trvsmartin@gmail.com>
2024-07-06 20:05:19 -0700

    fix: respect dynamic wind position

--- Assets/Scripts/SailBoatMovement.cs
+++ Assets/Scripts/SailBoatMovement.cs
@@ -6,40 +6,40 @@ public class SailBoatMovement : MonoBehaviour
     public Rigidbody2D rb; // Reference to the Rigidbody2D component
     
     public float rotationSpeed = 100f;
-    public float degrees=0; // track degrees to compare boat angle to wind
+    // public float degrees=0; // track degrees to compare boat angle to wind
     public float speedMultiplier; // based on degrees (points of sail)
     public AreaEffector2D waterWind; // base scene area effector; speed of 1
 
     public ParticleSystem[] wakeEffect;
     private Vector2 windDirection;
-    private float boatAngle;
+    // private float boatAngle;
     private float relativeWindAngle;
 
     Vector2 movement; // Vector to store the movement direction
 
     void Start()
     {
-        waterWind=GameObject.FindGameObjectWithTag("Force").GetComponent<AreaEffector2D>();
+
     }
 
     void Update()
     {
         UpdateWindDirection();
-        if(degrees==360|degrees==-360){
-            degrees=0;
-        }
+        CalculateRelativeWindAngle();
+        // if(degrees==360|degrees==-360){
+        //     degrees=0;
+        // }
 
-        var waterWindDirection = waterWind.forceAngle;
-        var degreesCompare = degrees - waterWindDirection;
+        var degreesCompare = relativeWindAngle;
 
         // Debug.Log(degreesCompare);
 
         speedMultiplier = degreesCompare switch
         {
-            var d when (d > -30 && d < 30)||(d>330)||(d<-330) => 0.3f,
-            var d when (d >= 30 && d < 60) || (d > -60 && d <= -30)||(d>=270)||(d<=-270) => 1f,
-            var d when (d >= 60 && d < 120) || (d > -120 && d <= -60)|| (d>=210)||(d<=-210)=> 2f,
-            var d when (d >= 120 && d < 150) || (d > -150 && d <= -120) ||(d>=150)||(d<=-150)=> 1.5f,
+            var d when (d > -30 && d < 30)=> 1.7f,
+            var d when (d >= 30 && d < 90) || (d > -90 && d <= -30)=> 2.2f,
+            var d when (d >= 90 && d < 150) || (d > -150 && d <= -90)=> 1.2f,
+            var d when (d>=150)||(d<=-150)=> 0.3f,
             _ => 0.1f
         };
         // Input
@@ -68,8 +68,8 @@ public class SailBoatMovement : MonoBehaviour
         
         // Calculate the amount of rotation
         float rotationAmount = movement.y * rotationSpeed * Time.deltaTime;
-        degrees+=rotationAmount;
-        // Debug.Log($"{degrees}, {speedMultiplier}");
+        // degrees+=rotationAmount;
+        Debug.Log($"{relativeWindAngle}, {speedMultiplier}");
         
         // Apply the rotation to the Rigidbody2D
         rb.MoveRotation(rb.rotation + rotationAmount);
@@ -79,7 +79,7 @@ public class SailBoatMovement : MonoBehaviour
     {
         Vector2 boatDirection=rb.transform.up;
         relativeWindAngle=Vector2.SignedAngle(boatDirection,windDirection);
-        Debug.Log(relativeWindAngle);
+        // Debug.Log(relativeWindAngle);
     }
     void UpdateWindDirection()
     {
